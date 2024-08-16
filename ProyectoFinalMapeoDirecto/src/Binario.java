26a08f844419b7bc901a5e90af6f2d4621e94416
KevinKeyPR <k3vin.h3rn4nd3zceped@gmail.com>
2024-08-15 21:25:22 -0500

    Update Binario.java

--- ProyectoFinalMapeoDirecto/src/Binario.java
+++ ProyectoFinalMapeoDirecto/src/Binario.java
@@ -45,6 +45,43 @@ public class Binario {
     public String getNumeroBinario() {
         return numeroBinario;
     }
+    public String getNumeroBinario() {
+        return numeroBinario;
+    }
+    public int convertirADecimal() {
+        if (numeroBinario.length() != 6) {
+            throw new IllegalArgumentException("El número binario debe tener 6 dígitos");
+        }
 
+        boolean isNegative = numeroBinario.charAt(0) == '1';
+
+        int resultado = 0;
+        if (isNegative) {
+
+            StringBuilder complemento = new StringBuilder();
+            for (int i = 0; i < 6; i++) {
+                complemento.append(numeroBinario.charAt(i) == '0' ? '1' : '0');
+            }
+
+     
+            for (int i = 1; i < 6; i++) {
+                if (complemento.charAt(i) == '1') {
+                    resultado += Math.pow(2, 5 - i);
+                }
+            }
+
+
+            resultado = -(resultado + 1);
+        } else {
+  
+            for (int i = 1; i < 6; i++) {
+                if (numeroBinario.charAt(i) == '1') {
+                    resultado += Math.pow(2, 5 - i);
+                }
+            }
+        }
+
+        return resultado;
+    }
 
 }
